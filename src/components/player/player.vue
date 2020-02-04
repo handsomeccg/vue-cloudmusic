ca3cd4bc2724abdeca3bf906e3f9c1c0c7bd0d7c
郑郑一锋 <yifeng.zheng@xiaohuodui.cn>
2020-02-05 02:35:35 +0800

    播放器组件优化，添加歌词，设置自动滚动和手动选择歌词播放

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -12,7 +12,25 @@
         </div>
       </div>
       <div class="middle">
-        <div class="song-cover" :class="`song-cover-${coverClass}`" :style="`background-image: url('${currentSong.picUrl}')`"></div>
+        <div v-show="!showLyric" class="song-cover" :class="`song-cover-${coverClass}`" :style="`background-image: url('${currentSong.picUrl}')`"></div>
+        <scroll v-show="showLyric" class="lyric" ref="lyric" :data="lyricList">
+          <div class="lyric-wrapper" @touchstart.prevent="lyricTouchStart" @touchmove.prevent="lyricTouchMove" @touchend.prevent="lyricTouchEnd">
+            <p
+              v-for="(item, index) in lyricList"
+              :key="index"
+              ref="lyricText"
+              class="lyric-text"
+              :class="{'lyric-active': index === currentLyricIndex}"
+            >
+              {{item.text}}</p>
+          </div>
+        </scroll>
+        <div class="move-play" v-show="this.touch.initiated">
+          <svg class="icon play" aria-hidden="true" @click="getLyricPosition">
+            <use xlink:href="#icon-bofang1"></use>
+          </svg>
+          <div class="divide" ref="divide"></div>
+        </div>
       </div>
       <div class="bottom">
         <div class="progress-wrapper">
@@ -67,13 +85,15 @@ import { playMode } from '@/assets/js/config'
 import { shuffle } from '@/assets/js/util'
 import { getLyric } from '@/api/song'
 import { lyricFormat } from '@/assets/js/lyricFormat'
+import scroll from '@/base/scroll/scroll'
 
 export default {
   name: 'player',
   created () {
-    console.log(this.playlist)
+    this.touch = {}
+    // console.log(this.playlist)
   },
-  components: { progressBar, progressCircle },
+  components: { progressBar, progressCircle, scroll },
   computed: {
     coverClass () {
       return this.playing ? 'play' : 'stop'
@@ -104,7 +124,11 @@ export default {
         '列表循环',
         '单曲循环',
         '随机播放'
-      ]
+      ],
+      lyricList: [],
+      showLyric: true,
+      currentLyricIndex: 0,
+      playLyric: 0
     }
   },
   methods: {
@@ -112,7 +136,62 @@ export default {
       this.setFullScreen(false)
     },
     updateTime (e) {
-      this.currentTime = e.target.currentTime
+      const currentTime = e.target.currentTime
+      this.currentTime = currentTime
+      // 滑动歌词的时候不做自动滚动
+      if (!this.touch.initiated) {
+        this.updateCurrentLyric(currentTime)
+      }
+    },
+    updateCurrentLyric (currentTime) {
+      // 实时记录歌词位置
+      for (let i = 0; i < this.lyricList.length; i++) {
+        // 歌曲刚开始
+        if (currentTime < this.lyricList[0].time) {
+          break
+        }
+        // 预防到最后一句歌词数组越界
+        if (i === this.lyricList.length - 1) {
+          this.currentLyricIndex = i
+          break
+        }
+        if (currentTime >= this.lyricList[i].time && currentTime < this.lyricList[i + 1].time) {
+          this.currentLyricIndex = i
+          break
+        }
+      }
+      // 实时滚动歌词
+      this.scrollLyric()
+    },
+    scrollLyric () {
+      // 滚动歌词
+      const el = this.$refs.lyricText
+      this.$refs.lyric.scrollToElement(el[this.currentLyricIndex], 1000, false, -160)
+    },
+    // 获取手动播放歌词位置 并跳转播放
+    getLyricPosition () {
+      const playPosition = this.$refs.divide.getBoundingClientRect().top
+      const el = this.$refs.lyricText
+      // 获取点击的歌词位置
+      for (let i = 0; i < el.length; i++) {
+        if (i === el.length - 1) {
+          this.playLyric = i
+          break
+        } else if (el[i].getBoundingClientRect().top <= playPosition && el[i + 1].getBoundingClientRect().top >= playPosition) {
+          this.playLyric = i
+          break
+        }
+      }
+      console.log(this.playLyric)
+      // 跳转到对应歌词
+      this.currentLyricIndex = this.playLyric
+      this.scrollLyric()
+      // 跳转到对应播放位置
+      const newTime = this.lyricList[this.playLyric].time
+      this.$refs.audio.currentTime = newTime
+      // 页面更新为正在播放
+      this.$refs.audio.play()
+      this.setPlayState(true)
     },
     changePercent (newPercent) {
       this.$refs.audio.currentTime = this.currentSong.songTime * newPercent
@@ -131,6 +210,7 @@ export default {
       this.resetCurrentIndex(list)
       this.setPlaylist(list)
     },
+    // 切换随机播放后设置当前播放歌曲为现在播放的歌曲
     resetCurrentIndex (list) {
       let index = list.findIndex((item) => {
         return item.id === this.currentSong.id
@@ -143,13 +223,15 @@ export default {
       second = second < 10 ? '0' + second : second
       return '0' + minute + ':' + second
     },
+    // 歌曲放完
     end () {
       if (this.mode === playMode.loop) {
         this.loop()
       } else {
-        this.next()
+        this.nextSong()
       }
     },
+    // 单曲循环
     loop () {
       this.$refs.audio.currentTime = 0
       this.$refs.audio.play()
@@ -158,6 +240,7 @@ export default {
         this.currentLyric.seek(0)
       } */
     },
+    // 切换上一首
     prevSong () {
       // 歌曲还未加载完成不进行操作
       if (!this.songReady) {
@@ -206,6 +289,23 @@ export default {
     error () {
       this.songReady = true
     },
+    lyricTouchStart () {
+      this.touch.initiated = true
+      clearTimeout(this.timeout)
+      console.log(123)
+    },
+    lyricTouchMove () {
+      if (!this.touch.initiated) {
+      }
+    },
+    lyricTouchEnd () {
+      this.timeout = null
+      clearTimeout(this.timeout)
+      this.timeout = setTimeout(() => {
+        this.touch.initiated = false
+      }, 2000)
+      console.log(456)
+    },
     ...mapMutations({
       setFullScreen: 'SET_FULL_SCREEN',
       setPlaylist: 'SET_PLAYLIST',
@@ -223,15 +323,19 @@ export default {
         return
       }
       setTimeout(() => {
-        this.$refs.audio.play()
         // 页面更新为正在播放
+        this.$refs.audio.play()
         this.setPlayState(true)
         // 获取歌词
         getLyric({ id: newSong.id }).then(res => {
           let lyric = res.data.lrc.lyric
           // console.log(lyric)
           const lyricList = lyricFormat(lyric)
+          this.lyricList = lyricList
           console.log(lyricList)
+          // 歌词复位
+          this.currentLyricIndex = 0
+          this.$refs.lyric.scrollTo(0, 0, 1000)
         })
       })
     },
@@ -288,6 +392,8 @@ export default {
       }
       .middle {
         width 100%
+        height 400px
+        margin-top 70px
         .song-cover-play {
           animation rotate 25s linear infinite
         }
@@ -300,10 +406,41 @@ export default {
           width 300px
           height 300px
           border-radius 50%
-          margin 70px auto
+          margin 0 auto
+        }
+        .move-play {
+          width 100%
+          height 30px
+          position fixed
+          top 300px
+          left 10px
+          display flex
+          .divide {
+            width 80%
+            height 1px
+            background-color rgba(226, 226, 226, 0.64)
+            margin-top 7px
+          }
+        }
+        .lyric {
+          width 80%
+          margin 0 auto
+          box-sizing border-box
+          text-align center
+          font-size 14px
+          line-height 40px
+          height 100%
+          overflow hidden
+          .lyric-wrapper {
+            padding-top 162px
+          }
+          .lyric-active {
+            color white
+          }
         }
       }
       .bottom {
+        margin-top 20px
         .progress-wrapper {
           display flex
           padding 0 12px
@@ -314,7 +451,7 @@ export default {
           }
         }
         .actions {
-          margin-top 30px
+          margin-top 0px
           display flex
           justify-content space-around
           .icon {
